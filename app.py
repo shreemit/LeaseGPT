eb0d61ae7ce8c73ad56af0a5dfa8975522c0c2e7
shreemit <shreemit27@gmail.com>
2023-05-25 15:45:42 -0700

    OpenAI integration

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+import pickle
 
 st.set_page_config(page_title="LeaseGPT",page_icon=':shark:')
 
@@ -16,6 +22,9 @@ def main():
         st.write("You selected New York City.")
     else:
         st.write("You haven't selected anything yet.")
+
+    api_key = st.text_input('Please enter your OpenAI key')
+
 
     st.sidebar.title("Hello ")
     st.sidebar.write("This is your personal leasing agent")
