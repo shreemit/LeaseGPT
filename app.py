cb82fa5629b05b23a4dd1e397ec5cd235fd23a7f
shreemit <shreemit27@gmail.com>
2023-05-27 20:32:59 -0700

    Add question answer functionality

--- app.py
+++ app.py
@@ -5,17 +5,22 @@ from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
 import pickle
 import os
 from dotenv import load_dotenv
+from raw_strings import *
 
-st.set_page_config(page_title="LeaseGPT",page_icon=':shark:')
+st.set_page_config(page_title="🏡 LeaseGPT", page_icon=":shark:")
 
 
 def main():
-    st.title("LeaseGPT")
+    st.title("🚪🏡 LeaseGPT")
     st.write("Your AI Leasing Assistant")
-    selection = st.selectbox("Choose a city", ["Seattle", "LA", "San Francisco", "New York City"])
+    
+    selection = st.selectbox(
+        "Choose a city", ["Seattle", "LA", "San Francisco", "New York City"]
+    )
     if selection == "Seattle":
         st.write("You selected Seattle.")
     elif selection == "LA":
@@ -27,46 +32,80 @@ def main():
     else:
         st.write("You haven't selected anything yet.")
 
-    api_key = st.text_input('Please enter your OpenAI key')
+    api_key = st.text_input("Please enter your OpenAI key")
     if api_key:
         os.environ["OPENAI_API_KEY"] = api_key
 
     load_dotenv()
 
-    if os.environ['OPENAI_API_KEY'] is not None:
-        print("OPEN AI Key",os.environ['OPENAI_API_KEY'])
+    if os.environ["OPENAI_API_KEY"] is not None:
+        print("OPEN AI Key", os.environ["OPENAI_API_KEY"])
+        # TODO: Scraping Craigslist
 
-        template = """Question: {question}
+        # Combinig all the text into one string
+        text = " ".join([doc1, doc2, doc3, doc4])
 
-        Answer: """
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=50, length_function=len
+        )
+
+        docs = [doc1, doc2, doc3, doc4]
+
+        chunks = []
+
+        # # Splitting the text into chunks
+        for doc in docs:
+            if len(doc) > 1000:
+                chunk_doc = text_splitter.split_text(doc)
+                for chunk in chunk_doc:
+                    chunks.append(chunk)
+            else:
+                chunks.append(doc)
+
+        store_name = "craigslist"
+        template =  '''I want you to act to act like a leasing agent for me. Giving me the best options always based on what you read below. 
+        You can give me something which matches my criteria or something which is close to it.
+        Question: {question}
+        Answer:
+        '''
         prompt = PromptTemplate(template=template, input_variables=["question"])
-        try: 
-            davinci = OpenAI(model_name='text-davinci-003', openai_api_key=api_key)
+        # print("Prompt", prompt)
+        try:
+            if os.path.exists(f"{store_name}.pkl"):
+                with open(f"{store_name}.pkl", "rb") as f:
+                    VectorStore = pickle.load(f)
+                    # st.write("Embeddings Loaded from the Disk")
+            else:
+                embeddings = OpenAIEmbeddings()
+                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+                with open(f"{store_name}.pkl", "wb") as f:
+                    pickle.dump(VectorStore, f)
+                    # st.write("Embeddings Created and Saved to Disk")
 
-            llm_chain = LLMChain(
-                prompt=prompt,
-                llm=davinci
-            )
-
-            qs = [
-                {'question': "Which NFL team won the Super Bowl in the 2010 season?"},
-                {'question': "If I am 6 ft 4 inches, how tall am I in centimeters?"},
-                {'question': "Who was the 12th person on the moon?"},
-                {'question': "How many eyes does a blade of grass have?"}
-            ]
-            # llm_chain.generate(qs)
+            query = st.text_input("Ask your question")
+            if query:
+                docs = VectorStore.similarity_search(query, k=3)
+                davinci = OpenAI(model_name="text-davinci-003")
+                chain = load_qa_chain(llm=davinci, chain_type="stuff")
+                prompt.format(question=query)
+                question = prompt.format(question=query)
+                # st.write("Prompt", question)
+                # st.write("Docs", docs)
+                with get_openai_callback() as callback:
+                    response = chain.run(input_documents=docs, question=template)
+                    # st.write(chain)
+                    st.write("Cost for query", callback.total_cost)
+                    st.write(response)
+                print("Response", response)
 
         except:
-            st.write("Please enter a valid OpenAI API Key")
+            if os.environ["OPENAI_API_KEY"] is None:
+                st.write("Please enter a valid OpenAI API Key")
 
     st.sidebar.title("Hello ")
-    st.sidebar.write("This is your personal leasing agent")
+    st.sidebar.write("This is your personal leasing agent LeasingGPT")
+    st.sidebar.write("I can help you find the best apartment for you")
+    st.sidebar.write("Made by Shreemit0")
 
-
-    
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
---
+++ raw_strings.py
@@ -0,0 +1,75 @@
+doc1 = '''
+Title: $2,650 / 3br - 1000ft2 - 3 Bedroom Loft Available Now! Great Location! U District (U-District)
+Cost: 2650 
+Description:
+Our beautiful apartments have quality options to make you feel at home.
+Community Info:
+As Seattles premier UW off-campus housing, Campus View & Avalon on 7th offer a range of spacious, recently remodeled residences with different floor plan options. Choose from Studio, 1, 2 & 3 bedroom Seattle U District apartments featuring a prime location and desirable amenities including a controlled access garage, rooftop deck, and more. Our professional, on-site management and maintenance staff keeps everything running smoothly!
+Our Seattle U District apartments are just blocks away from the University of Washington, making it easy to commute to class and meet up with friends! Were walking distance to many popular eateries, coffee shops, bars and stores in the University District. Its also just a quick bus or car ride to the heart of downtown Seattle.
+Contact us today to see how you can call Campus View or Avalon, the top student housing in Seattle, your home away from home! Our office can answer any questions you may have and get you scheduled for a tour.
+Campus View has two unique UW off-campus housing options located right next door to one another.
+Campus View I: Offering 2 and 3 bedroom homes, complete with granite counter tops, vinyl flooring, large bedrooms and spacious closets. Some of our University District apartments even feature spectacular views of The Space Needle, Lake Union, Mt. Rainier and Downtown.
+Campus View II: Offering1, 2 and 3 bedroom homes, complete with stone counter tops, stainless steel appliances, vinyl flooring, spacious closets, and private balconies. Some of the options also feature vaulted ceilings and a skylight in the living room.
+Just steps from Campus View you will find Avalon on 7th.
+Avalon on 7th: Offering Studio, 1 Bedroom, 2 Bedroom and 3 Bedroom homes, complete with new appliances, new flooring, new lighting, and some even offer balconies and lofts. These spacious open layouts will make you feel right at home!
+You won't find better UW off-campus student housing in Seattle!
+'''
+
+doc2 = '''
+Title: University District  3bd/2ba - M&J ~ close to UW!
+Cost: 2795
+Description:
+Short Term Lease - Available Available Spring/Summer Only - Schedule a Tour Today!
+
+3 Bedroom & 1.5 Bathroom Apartment
+
+For more information and to Schedule a tour please reply directly to this ad or contact show contact info .
+
+Caprice Apartments is conveniently located, just west of the University of Washington campus and blocks from all the conveniences of the university district.
+
+* North Facing Patio
+* All Vinyl Flooring - No Carpet
+* Short walking distance to University of Washington campus
+* Intercom Entry
+* Easy Bus Access
+* Onsite laundry
+* Spacious Bedrooms & Closet Space
+* Internet & Cable TV ready for service set up with provider
+* Garage resident parking is available $100 - $150 per month
+
+For more information please reply directly to this ad or contact show contact info .
+do NOT contact me with unsolicited services or offers
+'''
+
+doc3 ='''
+Title:  3 Bedroom 1.5 Bath Pre-lease for Fall 2023 $2350-$2450 (Seattle, WA)
+Cost: 2400
+Description:
+Please excuse the mess (these units are currently occupied), it will be cleaned before you move in.
+Our building is within walking distance to UW, a quick jog or bike ride to Greenlake and on a direct bus-line to downtown Seattle. Our units have recently been remodeled with all new cabinets, appliances (including a dishwasher and microwave) and fixtures! Some units have upgraded vinyl plank flooring as well. We have an elevator, laundry facilities and bike rack, and we also offer 1 FREE garage parking space. Sorry we do not accept pets or smoking.
+3 Bedroom 1.5 Bathroom $2350-2450 depending on the floor you are on
+There is a $40 credit checking fee per person, and we require a yearly lease, with last month's rent and a security deposit for those qualified to move in.
+Please feel free to reply to this ad or call show contact info and we can send pictures and a video tour and if you are interested we can arrange a showing of the unit.
+do NOT contact me with unsolicited services or offers
+'''
+
+doc4 = '''
+Title: Proximity to UVillage shopping district, Air Conditioning in all homes
+Cost: 5336
+Description:
+Two-story penthouse with three decks including your own private rooftop deck!
+Perfect location and just in time for spring and summer!
+- Secure elevator
+- Gas range
+- Full size washer and dryer!
+- Air conditioning
+- Secured garage parking
+- Private rooftop deck
+- 2 stories
+- Gorgeous views!
+Enjoy spectacular views and a great neighborhood at The Pacific Apartments. Located in the Ballard/Fremont/Wallingford area, we are situated alongside the Burke Gilman Trail, and just blocks from Gas Works Park, Woodland Park, the University of Washington, Interstate 5 Express Lanes, shopping, dining, entertainment, and much more!
+Tenant pays all utilities, one year lease.
+Check out our website to see Matterport 3D Tour and floorplans!
+https://pacific-apartments.com/property/unit-408/
+Call show contact info or show contact info to make this your next home!
+'''
